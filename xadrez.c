1bc71ce83a7dc13ead067c6d6a33747644af94ad
gpsdick <gpsdick@gmail.com>
2025-08-14 01:24:09 -0300

    Update xadrez.c
    xadrez nivel aventureiro

--- xadrez.c
+++ xadrez.c
@@ -1,25 +1,32 @@
-#include <stdio.h>
-
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    const int iTorre=5, iBispo=5, iRainha=-8, iCavalo=-1; // numeros positivos=cima/direita, negativos=esquerda/baixo
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
 
+    int i=0;
+    for(i;i<iBispo;i++)printf("Cima\nDireita\n");
+
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+
+    i=0;
+    do {printf("Direita\n");i++;}while(i<iTorre);
 
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
 
+    i=iRainha;
+    while(i<0){printf("Esquerda\n");i++;}
+
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
+
+    i=iCavalo;
+    for(i;i<0;i++)printf("Baixo\n");
+
+    i=iCavalo;
+    while(i<0){printf("Esquerda\n");i++;}
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
